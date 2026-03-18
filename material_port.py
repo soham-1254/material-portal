a2751990f965045527ef588faf2e13fa6055b165
soham-1254 <130497465+soham-1254@users.noreply.github.com>
2026-03-18 10:56:49 +0530

    Update material_port.py

--- material_port.py
+++ material_port.py
@@ -18,7 +18,8 @@ ADMIN_EMAILS = [
     "soham.panda@jute-india.com",
     "anushka.dutta@jute-india.com",
     "nitin.pandey@jute-india.com",
-    "prakhar.chandel@jute-india.com"
+    "prakhar.chandel@jute-india.com",
+    "payal.sinha@jute-india.com"
 ]
 
 REQUEST_FILE = "material_requests.xlsx"
